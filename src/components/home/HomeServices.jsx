1ebf091f4ae7911bc78cd80021888529201018d3
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-08-19 12:42:48 +0330

    fixing

--- src/components/home/HomeServices.jsx
+++ src/components/home/HomeServices.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import profile from "../../assets/images/background.png"
 
 export default function HomeServices({}){
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-full p-5">
             <div>
                 <div className="flex">
+                <div className="basis-1/8">
+                    <img src={profile} alt="" className="w-22"/>
+                </div>
                     <div className="basis-7/8">
                         
                     </div>
-                    <div className="basis-1/8"></div>
                 </div>
             </div>
         </div>

e3faf3e1eb24c86e09616086aeeb3f3801af3e1a
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-08-19 15:45:33 +0330

    ui-fixing

--- src/components/home/serviceBox/serviceGallery.jsx
+++ src/components/home/serviceBox/serviceGallery.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import GalleryImg from "../../../assets/images/modern-beauty-salon-interior.jpg"
+import GalleryImg from "../../../assets/images/modern-beauty-salon-interior.jpg";
 
-export default function ServiceGallery({}){
-    return(
-        <div className="border border-1 p-2 mt-4 rounded-xl flex">
-            <div className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2" style={{'backgroundImage':`url(${GalleryImg})`}}>
-            </div>  
-            <div className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2" style={{'backgroundImage':`url(${GalleryImg})`}}>
-                
-            </div>
-            <div className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2" style={{'backgroundImage':`url(${GalleryImg})`}}>
-                
-                </div>
-                <div className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2" style={{'backgroundImage':`url(${GalleryImg})`}}>
-                
-                </div>
-        </div>
-    )
+export default function ServiceGallery({}) {
+  return (
+    <div className="border border-1 p-2 mt-4 rounded-xl flex">
+      <div
+        className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2"
+        style={{ backgroundImage: `url(${GalleryImg})` }}
+      ></div>
+      <div
+        className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2"
+        style={{ backgroundImage: `url(${GalleryImg})` }}
+      ></div>
+      <div
+        className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2"
+        style={{ backgroundImage: `url(${GalleryImg})` }}
+      ></div>
+      <div
+        className="basis-1/4 h-80 rounded-xl bg-cover bg-center mx-2"
+        style={{ backgroundImage: `url(${GalleryImg})` }}
+      ></div>
+    </div>
+  );
 }

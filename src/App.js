65382ca316619057b961630a5f527ee7fc3366d1
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-08-19 12:32:27 +0330

    setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import HomeServices from './components/home/HomeServices';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App h-screen">
+        <div className='flex flex-row h-full'>
+        <div className='basis-5/6'>
+          <HomeServices /> 
+          </div>
+          <div className='basis-1/6'>
+            <Navbar />
+          </div>
+   
+        </div>
     </div>
   );
 }
---
+++ src/components/home/HomeServices.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function HomeServices({}){
+    return (
+        <div className="w-full h-full">
+            <div>
+                <div className="flex">
+                    <div className="basis-7/8">
+                        
+                    </div>
+                    <div className="basis-1/8"></div>
+                </div>
+            </div>
+        </div>
+    )
+}

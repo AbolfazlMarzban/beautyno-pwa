488c972de05261adc73519f4a0608f39b48b82e2
Abolfazl Marzban <abolfazlmarzban39@gmail.com>
2023-08-26 16:47:45 +0330

    form

--- src/components/profile/profileOrders/pOrderItem.jsx
+++ src/components/profile/profileOrders/pOrderItem.jsx
@@ -3,25 +3,24 @@ import profile from "../../../assets/images/background.png"
 
 export default function POrderItem() {
   return (
-    <div className="w-full border border-1 rounded-xl p-2 px-6 my-4">
+    <div className="w-full border border-1 rounded-xl p-2 px-2 my-4">
     <ul className="flex items-center justify-between ">
         <li>
-            <div className="h-16 w-16 rounded-full bg-cover bg-center" style={{'backgroundImage': `url(${profile})`}}>
+            <div className="h-8 w-8 rounded-full bg-cover bg-center" style={{'backgroundImage': `url(${profile})`}}>
             </div>
         </li>
         <li>
-            <span className="text-lg">اسم آرایشگر</span>
+            <span className="text-md">اسم آرایشگر</span>
         </li>
         <li>
-            <span className="text-lg">کوتاهی مو</span>
+            <span className="text-md">کوتاهی مو</span>
         </li>
         <li>
-            <span className="text-lg">روز دوشنبه 1402/5/27</span>
+            <span className="text-md">دوشنبه 1402/5/27</span>
         </li>
         <li>
-            <span className="text-lg">از ساعت 6 الی 8</span>
+            <span className="text-md">18:00 - 20:00</span>
         </li>
-        
     </ul>
 </div>  
     )
--- src/components/profile/profileServices/profileServices.jsx
+++ src/components/profile/profileServices/profileServices.jsx
@@ -1,70 +1,100 @@
-import React, { useState } from 'react'
-import Navbar from '../../Navbar'
-import PServiceItems from './pServiceItems'
+import React, { useState } from "react";
+import Navbar from "../../Navbar";
+import PServiceItems from "./pServiceItems";
 
 export default function ProfileServices() {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div className='flex flex-col h-full justify-center'>
-     
-    <div className='flex flex-col items-center justify-center'>
+    <div className="flex flex-col h-full justify-center">
+      <div className="flex flex-col items-center justify-center">
         <div>
-        <h2 className="text-xl font-bold">خدمات شما</h2>
+          <h2 className="text-xl font-bold">خدمات شما</h2>
         </div>
         <PServiceItems />
         <button
-        className="mt-2 w-4/5 bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        افزودن خدمت
-      </button>
-      {showModal ? (
-        <>
-          <div className="animate__animated animate__fadeInUp justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-x-0 bottom-0 z-50 outline-none focus:outline-none h-4/5">
-            <div className="relative w-full max-w-3xl h-full">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none h-full">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-2xl font-semibold">لیست خدمات</h3>
-                  <button
-                    className="p-1 border-0 text-red-600 text-xl font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    X
-                  </button>
-                </div>
-                {/*body*/}
-                <div className="relative p-6 flex-auto w-full">
-              
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  {/* <button
+          className="mt-2 w-4/5 bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          افزودن خدمت
+        </button>
+        {showModal ? (
+          <>
+            <div className="animate__animated animate__fadeInUp justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-x-0 bottom-0 z-50 outline-none focus:outline-none h-4/5">
+              <div className="relative w-full max-w-3xl h-full">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none h-full">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                    <h3 className="text-2xl font-semibold">لیست خدمات</h3>
+                    <button
+                      className="p-1 border-0 text-red-600 text-xl font-semibold outline-none focus:outline-none"
+                      onClick={() => setShowModal(false)}
+                    >
+                      X
+                    </button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative p-6 flex-auto w-full">
+                    <form>
+                      <div className="mb-6">
+                        <label
+                          for="name"
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          نام خدمت
+                        </label>
+                        <input
+                          type="text"
+                          id="name"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          placeholder="نام خدمت خود را وارد کنید..."
+                          required
+                        />
+                      </div>
+                      <div className="mb-6">
+                        <label
+                          for="password"
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          Your password
+                        </label>
+                        <input
+                          type="password"
+                          id="password"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          required
+                        />
+                      </div>
+                    </form>
+                  </div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                    {/* <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
                   </button> */}
-                  <button
-                    className="w-full bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    ذخیره خدمت
-                  </button>
+                    <button
+                      className="w-full bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      ذخیره خدمت
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
-     </div>   
-     <div className="w-full">
-            <Navbar />
-          </div>
-    </div>  )
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
+      </div>
+      <div className="w-full">
+        <Navbar />
+      </div>
+    </div>
+  );
 }

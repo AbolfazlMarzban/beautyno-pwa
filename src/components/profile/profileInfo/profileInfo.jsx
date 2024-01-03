4406d0995b725b511c83e6b142d67231135376f3
Abolfazl <abolfazlmarzban39@gmail.com>
2024-01-03 22:24:25 +0330

    personal info

--- src/components/profile/profileInfo/profileInfo.jsx
+++ src/components/profile/profileInfo/profileInfo.jsx
@@ -1,23 +1,24 @@
-import Navbar from "../../navbar"
+import Navbar from "../../navbar";
 
-
-export default function ProfileInfo(){
-    return(
-        <div className="flex flex-col h-full items-center justify-center">
-        <div className="flex flex-col items-center justify-center w-full max-w-3xl">
-          <div className="w-full flex flex-col items-start gap-2 justify-center">
+export default function ProfileInfo() {
+  return (
+    <div className="flex flex-col h-full items-center justify-center">
+      <div className="flex flex-col items-center justify-center w-full max-w-3xl">
+        <div className="w-full flex flex-col items-start gap-2 justify-center">
           <label htmlFor="">Full Name</label>
-          <input type="text" className="p-2 border rounded-lg w-full"/>
+          <input type="text" className="p-2 border rounded-lg w-full" />
           <label htmlFor="">Phone Number</label>
-          <input type="text" className="p-2 border rounded-lg w-full"/>
+          <input type="text" className="p-2 border rounded-lg w-full" />
           <label htmlFor="">Email</label>
-          <input type="text" className="p-2 border rounded-lg w-full"/>
-            <button className="bg-green-400 p-3 rounded-lg text-white w-full mt-4">Save</button>
+          <input type="text" className="p-2 border rounded-lg w-full" />
+          <button className="bg-green-400 p-3 rounded-lg text-white w-full mt-4">
+            Save
+          </button>
         </div>
-        </div>
-        <div className="w-full">
+      </div>
+      <div className="w-full">
         <Navbar />
       </div>
-        </div>
-    )
+    </div>
+  );
 }

1c79173e03e3122dbf0f52b5d98fe816f32c764f
Abolfazl <abolfazlmarzban39@gmail.com>
2024-01-03 22:23:07 +0330

    personal info

--- src/components/profile/profileInfo/profileInfo.jsx
+++ src/components/profile/profileInfo/profileInfo.jsx
@@ -5,8 +5,14 @@ export default function ProfileInfo(){
     return(
         <div className="flex flex-col h-full items-center justify-center">
         <div className="flex flex-col items-center justify-center w-full max-w-3xl">
-          <div className="w-full">
-
+          <div className="w-full flex flex-col items-start gap-2 justify-center">
+          <label htmlFor="">Full Name</label>
+          <input type="text" className="p-2 border rounded-lg w-full"/>
+          <label htmlFor="">Phone Number</label>
+          <input type="text" className="p-2 border rounded-lg w-full"/>
+          <label htmlFor="">Email</label>
+          <input type="text" className="p-2 border rounded-lg w-full"/>
+            <button className="bg-green-400 p-3 rounded-lg text-white w-full mt-4">Save</button>
         </div>
         </div>
         <div className="w-full">

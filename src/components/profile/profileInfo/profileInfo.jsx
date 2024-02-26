ff84218eb5d0199923c140acaa9f770268bc93be
Abolfazl <abolfazlmarzban39@gmail.com>
2024-02-26 10:07:30 +0330

    search page

--- src/components/profile/profileInfo/profileInfo.jsx
+++ src/components/profile/profileInfo/profileInfo.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
 import Navbar from "../../navbar";
 
 export default function ProfileInfo() {
+  const [profilePic, setProfilePic] = useState('')
+  function getProfilePic(ev){
+    var url = URL.createObjectURL(ev.target.files[0]);
+    setProfilePic(url);
+  }
   return (
     <div className="flex flex-col h-full items-center justify-center">
       <div className="flex flex-col items-center justify-center w-full max-w-3xl">
         <div className="w-full flex flex-col items-start gap-2 justify-center max-sm:p-3">
+        <div className="w-full h-32 border-2 border-dashed mt-2 rounded-lg flex justify-center items-center relative">
+                {profilePic.length > 0 ? (
+                  <img
+                    src={profilePic}
+                    alt=""
+                    className="w-full h-32 rounded-lg"
+                  />
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                    />
+                  </svg>
+                )}
+                <input
+                  type="file"
+                  accept="[.png, .jpg, .jepg]"
+                  className="w-full h-full opacity-0 absolute cursor-pointer"
+                  onChange={(ev) => getProfilePic(ev)}
+                />
+              </div>
           <label htmlFor="">Full Name</label>
           <input type="text" className="p-2 border rounded-lg w-full" />
           <label htmlFor="">Phone Number</label>
--- src/components/search/searchBox/searchBox.jsx
+++ src/components/search/searchBox/searchBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SearchBox() {
     return (
-        <div className='flex gap-3 p-4'>
+        <div className='flex justify-around p-4'>
             <div className='flex flex-col items-start gap-1'>
                 <label htmlFor="">Username</label>
                 <input type="text" className='border rounded-lg p-2' />
@@ -12,7 +12,11 @@ function SearchBox() {
                 <input type="text" className='border rounded-lg p-2' />
             </div>
             <div className='flex flex-col items-start gap-1'>
-                <label htmlFor="">Username</label>
+                <label htmlFor="">Service Day</label>
+                <input type="text" className='border rounded-lg p-2' />
+            </div>
+            <div className='flex flex-col items-start gap-1'>
+                <label htmlFor="">Service Time</label>
                 <input type="text" className='border rounded-lg p-2' />
             </div>
         </div>
